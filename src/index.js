8104ab28cb36da3fe1e6fc0567a32ad802dbaa27
Silich Alexander <sili41995@gmail.com>
2023-08-22 23:26:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import App from 'components/App';
 import theme from 'constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-react-hw-08-phonebook">
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </ThemeProvider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
