e04d04e0ddadd2e14358e44912a04903b786d75e
Silich Alexander <sili41995@gmail.com>
2023-08-23 08:32:10 +0300

    update

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import pagesPath from 'constants/pagesPath';
@@ -5,11 +6,19 @@ import { selectIsLoggedIn } from 'redux/auth/selectors';
 import { warnToast } from 'utils/toasts';
 
 export const PublicRoute = ({ element, restricted = false }) => {
+  const isFirstRenderRef = useRef(true);
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const location = useLocation();
   const shouldRedirect = isLoggedIn && restricted;
   const goBackPath = location.state?.from ?? `/${pagesPath.contactsPath}`;
-  location.state && !isLoggedIn && warnToast('Please, authenticate in the app');
+  const showWarnToast =
+    location.state && !isLoggedIn && isFirstRenderRef.current;
+  console.log(location.state);
+
+  useEffect(() => {
+    showWarnToast && warnToast('Please, authenticate in the app');
+    isFirstRenderRef.current = false;
+  }, [showWarnToast]);
 
   return shouldRedirect ? <Navigate to={goBackPath} /> : element;
 };
