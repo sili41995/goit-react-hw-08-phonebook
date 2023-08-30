5d0a4498b2a9b5283658ef8cd52c842dfb13b7e8
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-08-30 20:26:23 +0300

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => (
   <Message>
     This application was created to demonstrate my skills to potential employers
     skills in working with HTML, CSS, JavaScript, React, React Router, and Redux Toolkit. This application was created without the Axios library to improve
-    work skills with HTTP requests. Because under the hood of the React Redux
+    work skills with HTTP requests. Because under the hood of the Redux
     Toolkit is Immer - we can freely "mutate" the state, and Immer will do it
     correctly for us to update.
   </Message>
