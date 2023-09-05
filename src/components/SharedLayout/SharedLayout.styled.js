d07c64bba16d82fdc38d4a8110f3f2c687b195ff
Silich Alexander <sili41995@gmail.com>
2023-09-05 20:15:44 +0300

    update

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const Header = styled.header`
   display: inline-block;
+  min-width: 100%;
   padding-top: ${({ theme }) => theme.spacing * 5}px;
   padding-bottom: ${({ theme }) => theme.spacing * 5}px;
   background-color: ${({ theme }) => theme.primaryColor};
