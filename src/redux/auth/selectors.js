6c9d9d9393bd04642eae981b32fad15e98726953
Alexander Silich <sili41995@gmail.com>
2023-10-23 22:07:45 +0300

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -7,5 +7,3 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 
 export const selectIsLoading = (state) => state.auth.isLoading;
-
-export const selectIsLoadingg = (state) => state.auth.isLoading;
